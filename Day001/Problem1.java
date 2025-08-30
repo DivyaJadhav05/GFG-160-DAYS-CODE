cb88ff647a54166443fcf80c9f1005d9aabce545
DivyaJadhav05 <divyajadhav5335@gmail.com>
2025-08-30 19:15:19 +0530

    Problem1.java

--- Day001/Problem1.java
+++ Day001/Problem1.java
@@ -1,6 +1,34 @@
-// Day 1 - Solution placeholder
-public class Problem1 {
+import java.util.*;
+
+class Problem1 {
+    public static int getSecondLargest(int[] arr)
+    {
+        Arrays.sort(arr);
+        int largest = arr[arr.length-1];
+        for(int i = arr.length-2 ; i>=0 ; i--)
+        {
+            if(arr[i] != largest)
+            {
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Day 1 solution placeholder");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int result = getSecondLargest(arr);
+        System.out.println("Second Largest: " + result);
+        sc.close();
     }
 }
