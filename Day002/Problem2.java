19c2c83ed5505b850c1b014da56813fd7b3b87cc
DivyaJadhav05 <your-divyajadhav5335@gmail.com>
2025-08-31 18:29:11 +0530

    Updated Problem1.java and added Problem2.java

--- Day002/Problem2.java
+++ Day002/Problem2.java
@@ -1,6 +1,35 @@
-// Day 2 - Solution placeholder
-public class Problem2 {
-    public static void main(String[] args) {
-        System.out.println("Day 2 solution placeholder");
+import java.util.Scanner;
+class Solution 
+{
+    public static void pushZerosToEnd(int[] arr) 
+    {
+        int index=0;
+        for(int i=0; i<arr.length ; i++)
+        {
+            if(arr[i]!=0)
+            {
+                arr[index++]=arr[i];
+            }
+        }
+        while(index<arr.length)
+        {
+            arr[index++]=0;
+        }
+    }
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+        int size = obj.nextInt();
+        int[] arr = new int[size];
+        for(int i=0 ; i<size ; i++)
+        {
+            arr[i]=obj.nextInt();
+        }
+        pushZerosToEnd( arr);
+        for(int num : arr)
+        {
+            System.out.print(num+" ");
+        }
+        obj.close();
     }
 }
