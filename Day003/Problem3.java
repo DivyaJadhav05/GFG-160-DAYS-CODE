e60b1330cfca4d8693a3bfc7e5f074e6212cc53b
DivyaJadhav05 <your-divyajadhav5335@gmail.com>
2025-09-06 22:26:54 +0530

    Day 3: solved Problem3.java

--- Day003/Problem3.java
+++ Day003/Problem3.java
@@ -1,6 +1,32 @@
-// Day 3 - Solution placeholder
 public class Problem3 {
+    public void reverseArray(int arr[]) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Day 3 solution placeholder");
+        Problem3 sol = new  Problem3();
+
+        int arr[] = {1, 2, 3, 4, 5};
+
+        System.out.println("Original Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        sol.reverseArray(arr);
+
+        System.out.println("\nReversed Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
     }
 }
